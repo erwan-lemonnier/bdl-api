1f4976e1059e2b2bc40b9d7e63ce1f9729b2e1c8
Erwan Lemonnier <erwan@lemonnier.se>
2019-07-09 22:52:49 +0200

    Deploy script now works to create, update or test lambdas

---
+++ lambdas/deploy.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+
+set -e
+
+ARGS=$@
+
+ROOTDIR=$(git rev-parse --show-toplevel)
+cd $ROOTDIR
+
+if [ ! -e 'pym-config.yaml' ]; then
+    echo "ERROR: no pym-config.yaml in current dir"
+    exit 1
+fi
+
+NAME=$(pymconfig --name)
+
+usage() {
+    cat << EOF
+USAGE: lambdadeploy [--debug]
+
+Deploy BDL lambdas to amazon and setup their scheduler.
+
+OPTIONS:
+
+  --debug:          Be very verbose.
+  --create:         Create the lambda functions, instead of just updating them.
+  --test            Just test the lambdas.
+
+USAGE:
+
+./lambdadeploy
+
+EOF
+}
+
+WITH_DEBUG=
+DO_CREATE=
+DO_UPDATE=1
+DO_TEST=
+
+parse_args() {
+    while [ "$1" != "" ]; do
+        case $1 in
+            "--debug")         set -x; DEBUG='true'; WITH_DEBUG="--debug";;
+            "--create")        DO_CREATE=1; DO_UPDATE=;;
+            "--test")          DO_TEST=1;;
+            "-h" | "--help")   usage; exit 0;;
+            *)                 echo "Unknown argument '$1' (-h for help)"; exit 0;;
+        esac
+        shift
+    done
+}
+
+parse_args $ARGS
+
+AWS_PROFILE=$(pymconfig --aws-user)
+AWS_REGION=$(pymconfig --aws-region)
+
+# Prepare a tmp dir and clean it up
+TMPDIR=/tmp/lambda-deploy
+ZIPFILE=$TMPDIR/function.zip
+mkdir -p $TMPDIR
+# rm -rf $TMPDIR/venv
+rm -f $ZIPFILE
+
+echo "=> Root dir is $ROOTDIR"
+echo "=> Tmp dir is $TMPDIR"
+
+check_git_repo() {
+    IS_DIRTY_CLONE=$(git status --short --porcelain | wc -l)
+    if [ "$IS_DIRTY_CLONE" -gt 0 ]; then
+        echo "ERROR: $PWD is not clean! Commit and re-run."
+        exit 1
+    fi
+
+    GIT_DIFF_REMOTE=$(git diff master origin/master | wc -l)
+    if [ "$GIT_DIFF_REMOTE" -ne 0 ]; then
+        echo "ERROR: $PWD differs from origin. Please push to origin before releasing!"
+        exit 1
+    fi
+}
+
+create_virtenv() {
+    echo "=> Generating dependencies archive"
+    cd $TMPDIR
+    virtualenv -p python3 $TMPDIR/venv
+    source venv/bin/activate
+    pip install -r $ROOTDIR/requirements.txt
+    deactivate
+}
+
+build_zip() {
+    cd $TMPDIR/venv/lib/python3.4/site-packages
+    zip -r9 $ZIPFILE .
+    cd $ROOTDIR
+    zip -ur $ZIPFILE lambdas/*.py
+    zip -ur $ZIPFILE bdl/
+    zip -ur $ZIPFILE pym-config.yaml
+}
+
+# check_git_repo
+
+# Compile environment variables into one string
+SECRETS=""
+for VAR in $(pymconfig --env-secrets)
+do
+    echo "=> Adding $VAR"
+    VALUE=$(env | grep "^$VAR=" | cut -d '=' -f 2)
+    if [ -z "$VALUE" ]; then
+        echo "ERROR: variable $VAR has no value in env"
+        exit 1
+    fi
+    SECRETS="$VAR=$VALUE,$SECRETS"
+    echo "ESCRETS IS NOW [$SECRETS]"
+done
+SECRETS=$(echo $SECRETS | sed -e "s|,$||")
+
+echo "=> SECRETS=$SECRETS"
+
+# Invoke a lambda to test it
+test_lambda() {
+    LAMBDA_NAME=$1
+
+    # Test that the function was properly created and runs fine
+    echo "=> Invoking $LAMBDA_NAME in self-test mode"
+    aws lambda invoke \
+        --profile $AWS_PROFILE \
+        --function-name $LAMBDA_NAME \
+        --invocation-type RequestResponse \
+        --payload '{"action": "NOP"}' \
+        outfile
+
+    OK=$(cat outfile | grep '"statusCode": 200' | wc -l)
+    rm outfile
+    if [ $OK -ne 1 ]; then
+        echo "ERROR: self-test of $LAMBDA_NAME failed!"
+        exit 1
+    else
+        echo "=> Test successful!"
+    fi
+}
+
+
+# Create each lambda function
+create_lambda() {
+    LAMBDA_NAME=$1
+    METHOD_NAME=$2
+    cd $TMPDIR
+
+    # Delete the function if it already exists
+    echo "=> Checking if function $LAMBDA_NAME exists"
+    set +e
+    eval aws lambda get-function \
+         --profile $AWS_PROFILE \
+         --function-name $LAMBDA_NAME 2> /dev/null
+    RC=$?
+    set -e
+    if [ $RC == 0 ]; then
+        echo "=> Function $LAMBDA_NAME already exists. Deleting it first"
+        aws lambda delete-function \
+            --profile $AWS_PROFILE \
+            --function-name $LAMBDA_NAME
+    else
+        echo "=> Function $LAMBDA_NAME does not exist yet"
+    fi
+
+    # Create the function
+    echo "=> Creating lambda $LAMBDA_NAME"
+    aws lambda create-function \
+        --function-name $LAMBDA_NAME \
+        --region $AWS_REGION \
+        --zip-file fileb://function.zip \
+        --handler lambdas.lambdas.$METHOD_NAME \
+        --runtime python3.7 \
+        --role arn:aws:iam::579726532991:role/PymacaronLambda \
+        --tracing-config Mode=Active \
+        --timeout 300 \
+        --environment Variables="{$SECRETS}" \
+        --profile $AWS_PROFILE
+
+    # And make sure it's working
+    test_lambda $LAMBDA_NAME
+}
+
+update_lambda() {
+    LAMBDA_NAME=$1
+    cd $TMPDIR
+
+    # Stop if function does not exist
+    echo "=> Checking if function $LAMBDA_NAME exists"
+    eval aws lambda get-function \
+         --profile $AWS_PROFILE \
+         --function-name $LAMBDA_NAME
+
+    # Update the function's code
+    echo "=> Updating lambda $LAMBDA_NAME"
+    aws lambda update-function-code \
+        --function-name $LAMBDA_NAME \
+        --zip-file fileb://function.zip \
+        --profile $AWS_PROFILE
+
+    # And update its configuration
+    aws lambda update-function-configuration \
+        --function-name $LAMBDA_NAME \
+        --handler lambdas.lambdas.$METHOD_NAME \
+        --runtime python3.7 \
+        --role arn:aws:iam::579726532991:role/PymacaronLambda \
+        --tracing-config Mode=Active \
+        --timeout 300 \
+        --environment Variables="{$SECRETS}" \
+        --profile $AWS_PROFILE
+}
+
+if [ ! -z "$DO_TEST" ]; then
+    test_lambda udpate_sitemap
+    test_lambda scan_source
+
+else
+
+    create_virtenv
+    build_zip
+
+    if [ ! -z "$DO_CREATE" ]; then
+        create_lambda udpate_sitemap lambda_update_sitemap
+        create_lambda scan_source lambda_scan_source
+        # create_lambda clean_source lambda_clean_source
+
+    elif [ ! -z "$DO_UPDATE" ]; then
+        update_lambda update_sitemap
+        create_lambda scan_source
+
+    fi
+fi
+
+echo "=> Done."
---
+++ lambdas/lambdas.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import time
+import logging
+import requests
+import click
+import flask
+import traceback
+from pymacaron.config import get_config
+from pymacaron.auth import generate_token
+from pymacaron.utils import timenow, to_epoch
+from pymacaron.crash import set_error_reporter
+from pymacaron.crash import report_error
+from pymacaron.crash import populate_error_report
+
+
+log = logging.getLogger(__name__)
+
+
+app = flask.Flask(__name__)
+
+
+# Setup logging
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+# formatter = logging.Formatter('%(message)s')
+# handler.setFormatter(formatter)
+log.addHandler(handler)
+logging.getLogger('urllib3.connectionpool').setLevel(logging.INFO)
+
+
+PATH_LIBS = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+sys.path.append(PATH_LIBS)
+
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pym-config.yaml')
+get_config(config_path)
+
+from bdl.exceptions import bdl_error_reporter
+from bdl.io.slack import do_slack
+
+
+# If set, will override the default api|scraper.bazardelux.com target urls
+HOST = None
+
+
+def call_api(method, url, data=None, ignore_error=False):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer %s' % generate_token('scheduler', data={}, expire_in=86400)
+    }
+    log.info("Calling %s %s" % (method, url))
+    if method == 'POST':
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+    else:
+        r = requests.get(url, headers=headers)
+    log.info("=> Got: %s" % r.text)
+    if str(r.status_code) != '200':
+        raise Exception("Call to %s %s with data %s returned %s" % (method, url, data, r.text))
+    return r.json()
+
+
+#
+# SCAN
+#
+
+def scan_source(source=None, manual=False):
+    source = source.upper()
+    log.info("=> Lauching scan of source %s" % source)
+
+    domain = HOST if HOST else 'https://api.bazardelux.com'
+
+    # By default, scan 1 day back in time
+    epoch_oldest = to_epoch(timenow()) - 86600
+
+    # Find out epoch of last scan
+    j = call_api('GET', '%s/v1/search/latest?source=%s' % (domain, source), ignore_error=True)
+    if 'bdlitem' in j and 'epoch_published' in j['bdlitem']:
+        epoch_oldest = j['bdlitem']['epoch_published']
+        # If j is an error, just use the default epoch_oldest
+
+    # And launch a scan
+    date_oldest = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_oldest))
+    log.info("Scanning back to epoch %s (%s)" % (epoch_oldest, date_oldest))
+    call_api(
+        'POST',
+        'https://crawler.bazardelux.com/v1/crawler/scan',
+        {
+            'source': source,
+            'epoch_oldest': epoch_oldest,
+        },
+    )
+
+    do_slack(
+        "Scheduler: %slaunched scan of source %s back to %s" % ('manually ' if manual else '', source, date_oldest),
+        channel=get_config().slack_scheduler_channel,
+    )
+
+
+def update_sitemap(source=None, manual=False):
+    log.info("=> Lauching sitemap update")
+
+    domain = HOST if HOST else 'https://api.bazardelux.com'
+
+    call_api('GET', '%s/v1/bdl/sitemap/update' % domain)
+
+    do_slack(
+        "Scheduler: %supdated sitemap" % ('manually ' if manual else ''),
+        channel=get_config().slack_scheduler_channel,
+    )
+
+
+
+def clean_source(source=None, manual=False):
+    source = source.upper()
+    log.info("=> Cleaning up oldest announces from %s" % source)
+    # Count how many items are indexed from this source, and retrieve as many
+    # of them so that all will be scanned within a week
+    # Call v1/parse with the item's urls
+
+    do_slack(
+        "Scheduler: %slaunched cleanup of source %s" % ('manually ' if manual else '', source),
+        channel=get_config().slack_scheduler_channel,
+    )
+
+
+
+
+def generic_handler(f, event, manual=False):
+    """Execute a method while wrapping it in a pymacaron context.
+    Expects an event dict of the form:
+    {
+      source: <source>,  (optional)
+      test: True|False,  (optional)
+    }
+    """
+
+    with app.test_request_context(''):
+        # Configure the error reporter and force pymacaron to report errors
+        set_error_reporter(bdl_error_reporter)
+        os.environ['DO_REPORT_ERROR'] = "1"
+
+        try:
+            # Is it a custom test action?
+            if 'action' in event:
+                if event['action'] == 'NOP':
+                    log.info("Called in self-test mode. Doing nothing")
+                elif event['action'] == 'FAIL':
+                    raise Exception("Simulate an error")
+                else:
+                    raise Exception("Do not know how to handle action %s" % event['action'])
+            else:
+                # Just execute the callback
+                event['manual'] = manual
+                f(**event)
+
+        except Exception as e:
+            data = {}
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            trace = traceback.format_exception(exc_type, exc_value, exc_traceback, 30)
+            data['trace'] = trace
+            populate_error_report(data)
+            report_error(
+                title='%s CRASH: %s' % ('MANUAL' if manual else 'LAMBDA', f.__name__),
+                data=data,
+                caught=e,
+                is_fatal=True,
+            )
+
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': str(e)})
+            }
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'info': 'Executed %s' % f.__name__})
+    }
+
+
+#
+# Handle execution from within a lambda function
+#
+
+def lambda_scan_source(event, context):
+    return generic_handler(scan_source, event, manual=False)
+
+def lambda_clean_source(event, context):
+    return generic_handler(clean_source, event, manual=False)
+
+def lambda_update_sitemap(event, context):
+    return generic_handler(update_sitemap, event, manual=False)
+
+
+#
+# Handle command line execution
+#
+
+@click.command()
+@click.option('--scan', required=False, metavar='SOURCE', help="Scan the given source", default=False)
+@click.option('--clean', required=False, metavar='SOURCE', help="Remove sold announces from this source", default=False)
+@click.option('--sitemap/--no-sitemap', required=False, metavar='', help="Update the bazardelux sitemap", default=False)
+@click.option('--host', required=False, metavar='PROTO://HOST:PORT', help="Call this proto://host:port instead of the default bazardelux.com server", default=False)
+def main(scan, clean, sitemap, host):
+    """Scheduler for bazardelux.com, in charge of running crawlers at regular
+    intervals, polling items to remove them when they are sold and updating the
+    sitemaps every day.
+
+    Can also be used to launch a single scan, clean or sitemap operation.
+
+    Examples:
+
+    tictac                      # Start the scheduler and keep it running #
+    tictac --scan TRADERA       # Scan TRADERA                            #
+    tictac --clean TRADERA      # Purge the oldest sold ads from TRADERA  #
+    tictac --sitemap            # Update bazardelux's sitemap             #
+    tictac --sitemap --host http://127.0.0.1:8080
+
+    """
+
+    # Configure the error reporter and force pymacaron to report errors
+    set_error_reporter(bdl_error_reporter)
+    os.environ['DO_REPORT_ERROR'] = "1"
+
+    global HOST
+    if host:
+        HOST = host
+
+    #
+    # Check arguments and run single tasks
+    #
+
+    if scan or clean:
+        if scan and clean:
+            log.warn("You must provide only one of --scan or --clean")
+            sys.exit(1)
+        if scan:
+            generic_handler(scan_source, {'source': scan}, manual=True)
+            sys.exit(0)
+        if clean:
+            generic_handler(clean_source, {'source': clean}, manual=True)
+            sys.exit(0)
+    elif sitemap:
+        generic_handler(update_sitemap, {}, manual=True)
+        sys.exit(0)
+
+    log.warn("No action specified")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    with app.test_request_context(''):
+        main()
